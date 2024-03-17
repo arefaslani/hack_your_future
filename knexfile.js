622b2f3e98fdcc54a89a828ace499364fd3a2f98
Aref Aslani <arefaslani@gmail.com>
2024-03-17 20:14:43 +0100

    Create database configuration

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      port: 3306,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'hyf',
+    },
+  },
+}
